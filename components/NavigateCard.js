9b6aef3758bf28a4ddf94b3de50ae270a6ae9dad
NITHISH K MEGARAJAN <68291344+NithishK5@users.noreply.github.com>
2023-01-11 15:09:34 +0000

    map screen

---
+++ components/NavigateCard.js
@@ -0,0 +1,50 @@
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import {GOOGLE_MAPS_APIKEY} from '@env'
+
+
+const NavigateCard = () => {
+  return (
+    <SafeAreaView style={tw `bg-white flex-1`}>
+      <Text style={tw `text-center py-5 text-xl`}>Good Morning</Text>
+      <View style={tw `border-t border-gray-200 flex-shrink`}>
+        <View>
+            <GooglePlacesAutocomplete
+                placeholder='Where to?'
+                styles={toInputBoxStyles}
+                fetchDetails={true}
+                enablePoweredByContainer = {false}
+                query={{
+                    key: GOOGLE_MAPS_APIKEY,
+                    language: 'en',
+                }}
+                nearbyPlacesAPI='GooglePlacesSearch'
+                debounce={400}
+            />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default NavigateCard
+
+const toInputBoxStyles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        paddingTop: 20,
+        flex: 0,
+    },
+    textInput: {
+        backgroundColor: "#DDDDDF",
+        borderRadius: 0,
+        fontSize: 18,
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0,
+    },
+    
+})
