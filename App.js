07c7c1e9fc2f5bfa6f3c1f0363ff9776152d56e0
NITHISH K MEGARAJAN <68291344+NithishK5@users.noreply.github.com>
2023-01-11 16:59:50 +0000

    different rides added

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
@@ -8,6 +7,7 @@ import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MapScreen from './screens/MapScreen';
+import { KeyboardAvoidingView, Platform } from 'react-native';
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -16,33 +16,32 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
       <SafeAreaProvider>
-        <Stack.Navigator>
-          <Stack.Screen 
+        <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{flex:1}}
+        keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+        >
+          <Stack.Navigator>
+            <Stack.Screen 
           name="Home"
           component={HomeScreen} 
           options={{
             headerShown: false,
           }}
           />
-          <Stack.Screen 
+            <Stack.Screen 
           name="MapScreen"
           component={MapScreen} 
           options={{
             headerShown: false,
           }}
           />
-        </Stack.Navigator>
+          </Stack.Navigator>
+        </KeyboardAvoidingView>
       </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
